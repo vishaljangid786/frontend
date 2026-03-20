db9b871618222bbe3168164b78378390d75431c7
Vishal Jangid <vishaljangid80550786@gmail.com>
2026-03-20 09:03:02 +0530

    home screen ui completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:'#111111',
+        secondary:'#666666',
+        background:'#FFFFFF',
+        surface:'#F7F7F7',
+        accent:'#FF4C3B',
+        border:'#EEEEEE'
+      }
+    },
   },
   plugins: [],
 }
